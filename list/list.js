f9436ef1b1e2191739b2904635383dbda8ad6d64
艺璇 <mengxue.zmx@alibaba-inc.com>
2015-05-10 19:30:20 +0800

    fix list

--- list/list.js
+++ list/list.js
@@ -1,3 +1,27 @@
-(function(){
 
-})();
+function useType(type){
+	var $lazyModules = $("#"+type+" div");
+	$(window).on("scroll", function(){
+	      var sTop = $(window).scrollTop();
+	      var wH = $(window).height();
+	      $lazyModules.each(function(index, item){
+	           var $item = $(item);
+	           if(!$item.hasClass(type) && ($item.offset().top) < wH + sTop){
+	                $item.addClass(type);
+	                //$item.addClass(type).show();
+	           }
+	           if(($item.offset().top) < sTop || ($item.offset().top) > wH + sTop){
+	           		if($item.hasClass('load')){
+	           			$item.removeClass(type);
+	           		}else{
+	           			$item.addClass('load');
+	           		}
+	           }
+	      });
+	      if($(".loaded").length == $lazyModules.length){
+	           $(window).off("scroll");
+	      }
+	 }).trigger("scroll");
+}
+
+
